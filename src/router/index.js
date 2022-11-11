a4b9160d6463fb2204c7c6a329e8793845c570d6
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-11-12 01:21:05 +0900

    ADD: 動的ルーティング追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import NotFoundView from '../views/NotFoundView.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView,
+    meta: { title: 'Home' },
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
+    meta: { title: 'About' },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -25,6 +28,14 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "post" */ '../views/PostView.vue'),
   },
+  {
+    path: '/post/:id',
+    name: 'PostShow',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/PostShowView.vue'),
+    props: true,
+  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
 ];
 
 const router = new VueRouter({
@@ -32,4 +43,10 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
+});
+
 export default router;
